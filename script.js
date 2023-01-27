13f224d98e152c7f0b9d88157c1d747a5a67a2bf
Mario <mario@supermario.dk>
2023-01-27 17:22:12 +0100

    Added JS when but is pushed it wil output to console

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-ECHO is on.
+const NumberOfBTN= 9;
+let ArrayOfBTN = [];
+let num_1 = 0;
+let mum_2 = 0;
+for (let i = 0; i<=NumberOfBTN; i++){
+    const BTN = document.getElementById(i.toString());
+    ArrayOfBTN[i] = BTN;
+}
+
+console.log(ArrayOfBTN);
+
+for (let i = 0; i<=NumberOfBTN; i++){
+ArrayOfBTN[i].onclick = function(){
+    num_1 = ArrayOfBTN[i].innerHTML
+    console.log(num_1);
+
+}
+}
+
+
